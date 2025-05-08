b090a9383b981537e1b4d718e92b1f6335c329b9
Dowland Aiello <dowlandaiello@gmail.com>
2025-05-08 14:02:38 -0700

    Split script.

--- scripts/split_large_csv.R
+++ scripts/split_large_csv.R
@@ -1,27 +1,33 @@
 library(dplyr)
 library(purrr)
+library(stringr)
+library(readr)
 
 split_df_head_tail <- function(df, n) {
-  head <- df[1:n]
-  tail <- df[n:(df %>% nrow)]
+  head <- df %>% head(n)
+  tail <- df %>% tail(n = (df %>% nrow) - n)
 
-  c(head, tail)
+  list(chunk = head, acc = tail)
 }
 
 # N rows per output split file
 read_csv_gz <- function(path, chunk_size) {
-  df <- read.delim(path)
+  df <- path %>% read_csv(lazy = TRUE)
 
   # Find number of chunks, for each chunk, call split_df_head_tail
-  chunks <- (1:(df %>% nrow()) / chunk_size) |>
-    accumulate(\(i, accum) {
-      # accum[2] = current subset of df to split
-      # accum[1] = all dfs created
-      res <- accum[2] |>
+  # continually make df smaller, taking chunks of it of equal size
+  # each time
+  (1:((df %>% nrow) %/% chunk_size)) %>%
+    accumulate(.f = \(df, i) {
+      res <- df %>%
         split_df_head_tail(n = chunk_size)
 
-      c(accum[1] |> c(res[1]), res[2])
-    })
+      res[["chunk"]] %>% write.csv(str_c(path, "_", i, ".csv.gz"))
 
-  print(chumks[1] |> summarize)
+      res[["acc"]]
+    }, .init = df)
 }
+
+read_csv_gz(
+  "data/apprehensions_origins/Family Units apprehended along the SWB FY2000 Redacted_raw.xlsx.csv.gz",
+  5)
