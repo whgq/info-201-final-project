c8f6b55113afc697484e50feac39cc5988f56774
Dowland Aiello <dowlandaiello@gmail.com>
2025-05-20 10:30:20 -0700

    Update flake

--- flake.nix
+++ flake.nix
@@ -6,79 +6,80 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = nixpkgs.legacyPackages.${system};
-        deps = with pkgs; with rPackages; [
-          R
-          dplyr
-          ggplot2
-          readxl
-          readr
-          stringr
-          purrr
-          tidyr
-          ggmap
-          fuzzyjoin
-          maps
-          mapproj
-          scales
-          FNN
-          pracma
-          numDeriv
-          R
-          rmarkdown
-          dplyr
-          ggplot2
-          readr
-          stringr
-          purrr
-          tidyr
-        ];
-        knit = path: pkgs.stdenv.mkDerivation {
-          name = "knit";
-          src = ./.;
-          buildInputs = deps ++ (with pkgs; [
-            pandoc
-          ]);
-          # This produces ${name}.html
-          buildPhase = ''
-            ${pkgs.R}/bin/R -e "rmarkdown::render('${path}.Rmd', output_format = 'html_document')"
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        deps = with pkgs;
+          with rPackages; [
+            R
+            dplyr
+            ggplot2
+            readxl
+            readr
+            stringr
+            purrr
+            tidyr
+            ggmap
+            fuzzyjoin
+            maps
+            mapproj
+            scales
+            FNN
+            pracma
+            numDeriv
+            R
+            rmarkdown
+            dplyr
+            ggplot2
+            readr
+            stringr
+            purrr
+            tidyr
+          ];
+        knit = path:
+          pkgs.stdenv.mkDerivation {
+            name = "knit";
+            src = ./.;
+            buildInputs = deps ++ (with pkgs; [ pandoc ]);
+            # This produces ${name}.html
+            buildPhase = ''
+              ${pkgs.R}/bin/R -e "rmarkdown::render('${path}', output_format = 'html_document')"
+            '';
+            installPhase = ''
+              mkdir $out
+              mv ${path}.html $out/
+            '';
+          };
+        serve = name:
+          pkgs.writeShellScriptBin "serve" ''
+            ${pkgs.http-server}/bin/http-server ${knit name}/
           '';
-          installPhase = ''
-            mkdir $out
-            mv ${path}.html $out/
-          '';
-        };
-        serve = name: pkgs.writeShellScriptBin "serve" ''
-          ${pkgs.http-server}/bin/http-server ${knit name}/
-        '';
         serve-live = pkgs.writeShellScriptBin "serve-live" ''
-          ${pkgs.watchexec}/bin/watchexec -w "$1" --restart -- nix run .#"$1"
+          ${pkgs.watchexec}/bin/watchexec -w "$1" --restart -- nix run .#"$(basename $1)"
         '';
-    in
-    {
-      devShells.default = pkgs.mkShell {
-        nativeBuildInputs = deps ++ [ pkgs.rPackages.languageserver ];
-      };
-      apps = (with builtins;
-        let rmds = (filter
-          (fname: match ".*\\.Rmd$" fname != null)
-          (
-            (map (fname: "./src/heat_maps/${fname}") (attrNames (readDir ./src/heat_maps))) ++
-            (map (fname: "./src/data_exploration/${fname}") (attrNames (readDir ./src/data_exploration)))
-          )); in
-          listToAttrs (map (fname: {
-            name = fname;
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = deps ++ [ pkgs.rPackages.languageserver ];
+        };
+        apps = (with builtins;
+          let
+            rmds = (filter (fname: match ".*\\.Rmd$" fname != null)
+              ((map (fname: "./src/heat_maps/${fname}")
+                (attrNames (readDir ./src/heat_maps)))
+                ++ (map (fname: "./src/data_exploration/${fname}")
+                  (attrNames (readDir ./src/data_exploration)))));
+          in listToAttrs (map (fname: let ftrunk = builtins.replaceStrings [".Rmd"] [""] (baseNameOf fname); in {
+            name = baseNameOf fname;
             value = {
               type = "app";
-              program = "${serve fname}/bin/serve";
+              program = "${serve (baseNameOf fname)}/bin/serve";
             };
-      }) rmds)) // {
-        serve-live = {
-          type = "app";
-          program = "${serve-live}/bin/serve-live";
-        };
-      };
-    }
-  );
+          }) rmds)) // {
+            serve-live = {
+              type = "app";
+              program = "${serve-live}/bin/serve-live";
+            };
+          };
+      });
 }
